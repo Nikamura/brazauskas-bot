36a9f12a12afa97b82bb0d1802e78b48e4729118
Karolis Mažukna <me@karolis.dev>
2021-04-16 16:50:34 +0300

    feat: separate routes

--- src/setup-web.ts
+++ src/setup-web.ts
@@ -4,9 +4,28 @@ import { Service } from "./interfaces/common.interface";
 export async function setupWeb({ service: web, db }: Service<FastifyInstance>) {
   web.get("/", async () => {
     return {
+      downloads: "/downloads",
+      messages: "/messages",
+    };
+  });
+
+  web.get("/downloads", async () => {
+    return {
       posts: await db.download.findMany({
         orderBy: { id: "desc" },
         include: { message: { include: { replyToMessage: true } } },
+        take: 100,
+      }),
+    };
+  });
+
+  web.get("/messages", async () => {
+    return {
+      posts: await db.message.findMany({
+        orderBy: { id: "desc" },
+        where: { chatType: { equals: "supergroup" } },
+        include: { downloads: true },
+        take: 100,
       }),
     };
   });
